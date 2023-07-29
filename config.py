8c4a6c4ff20bf747bfd1d5eb3be4b8022096b49c
MassimoTambu <massimo.tamburini.work@gmail.com>
2023-07-29 09:37:34 +0200

    first commit

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import json
+
+f = open('bot_data.json', 'r')
+data = json.loads(f.read())
+f.close()
 # insert your bot token between quotes
-TOKEN = ''
+# RaccattaBot token
+TOKEN = data['token']
 # insert the tag of the channel where you want to send posts after the @
-chat_id = '@'
+chat_id = data['chat_id']
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import asyncio
 import telegram, threading
 from facebook_scraper import get_posts
 from datetime import datetime
@@ -20,14 +21,15 @@ fields = ['page_name', 'page_tag', 'last_post_used']
 
 WAIT_SECONDS = 600
 
-def check():
+async def check():
     with open('pages.csv', mode='r+') as csv_file, NamedTemporaryFile(mode='w', delete=False) as tempfile:
         csv_reader = csv.DictReader(csv_file)
         csv_writer = csv.DictWriter(tempfile, fields)
         csv_writer.writeheader()
         for page in csv_reader:
             try:
-                posts = list(get_posts(page['page_tag'], pages=2, cookies='cookies.txt'))
+                # https://github.com/kevinzg/facebook-scraper/issues/990
+                posts = list(get_posts(page['page_tag'], start_url="https://m.facebook.com/profile.php?id="+page['page_tag'], pages=2, cookies='cookies.txt'))
             except OSError as err:
                 print("Got an exception while trying to retrieve posts")
                 traceback.print_exc()
@@ -41,21 +43,24 @@ def check():
             for post in posts:
                 if int(post['post_id']) <= int(page['last_post_used']): # post already sent to channel
                     continue
-                post_text = telegram.utils.helpers.escape_markdown(post['post_text'], version=2) if post['post_text'] else ''
+                post_text = telegram.helpers.escape_markdown(post['post_text'], version=2) if post['post_text'] else ''
                 if post['shared_post_url'] is not None:
-                    shared_text = '_' + telegram.utils.helpers.escape_markdown(post['shared_text'][post['shared_text'].find('\n', post['shared_text'].find('\n')+1)+2:], version=2) + '_' if post['shared_text'] else ''
-                    message = u'\U0001F501' + ' ' + post['shared_username'] + '\n' + shared_text + '\n\n' + post_text + '\n\[' + page['page_name'] + '\]'
+                    shared_text = '_' + telegram.helpers.escape_markdown(post['shared_text'][post['shared_text'].find('\n', post['shared_text'].find('\n')+1)+2:], version=2) + '_' if post['shared_text'] else ''
+                    message = u'\U0001F501' + ' ' + post['shared_username'] + '\n' + shared_text + '\n\n' + post_text + '\n[' + page['page_name'] + '](https://www.facebook.com/groups/' + page['page_tag'] + ')'
                 else:
-                    message = post_text + '\n\[' + page['page_name'] + '\]'
+                    message = post_text + '\n[' + page['page_name'] + ']'
                 if post['images'] is not None and len(post['images']) > 0:
-                    images = [telegram.InputMediaPhoto(post['images'][0], caption=message, parse_mode=telegram.constants.PARSEMODE_MARKDOWN_V2)]
+                    images = [telegram.InputMediaPhoto(post['images'][0], caption=message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)]
                     for image in post['images'][1:]:
                         images.append(telegram.InputMediaPhoto(image))
-                    bot.send_media_group(chat_id, images)
+                    print("SEND IMAGE")
+                    await bot.send_media_group(chat_id, images)
                 elif post['video'] is not None:
-                    bot.send_video(chat_id, post['video'], caption=message)
+                    print("SEND VIDEO")
+                    await bot.send_video(chat_id, post['video'], caption=message)
                 elif post['text'] is not None:
-                    bot.send_message(chat_id, message, parse_mode=telegram.constants.PARSEMODE_MARKDOWN_V2)
+                    print("SEND MESSAGE")
+                    await bot.send_message(chat_id, message, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
             if len(posts) != 0: 
                 # according to facebook-scraper devs you can get 0 posts if
                 # you get temporarily ip banned for too many requests 
@@ -63,6 +68,7 @@ def check():
             row = {'page_name': page['page_name'], 'page_tag': page['page_tag'], 'last_post_used': page['last_post_used']}
             csv_writer.writerow(row)
     shutil.move(tempfile.name, 'pages.csv')
+    print("ASD")
     threading.Timer(WAIT_SECONDS, check).start()
 
-check()
+asyncio.run(check())
